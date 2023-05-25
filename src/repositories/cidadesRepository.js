51c5e1a63dc111653a3bc15b4777fa3140c26cc5
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-25 16:40:50 -0300

    Fix query selector

--- src/repositories/cidadesRepository.js
+++ src/repositories/cidadesRepository.js
@@ -10,22 +10,16 @@ async function createCidade(nome, estado_id){
 
 async function getCidades(nome){
 
-    const select = `SELECT 	"Cidades".nome as "Cidade", 
-                            "Hoteis".nome as "Hotel", 
-                            "Quartos".disponivel, 
-                            "Quartos".id as "Número_do_Quarto", 
-                            "Fotos".id as "Foto ID",
-                            "Fotos".foto as "url",
-                            "Rotas".destino as "Destino",
-                            "Companhia".nome as "Companhia",
-                            "Passagens".hora_saida as "Data_de_partida",
-                            "Passagens".hora_chegada as "Data_de_chegada",
-                            "Passagens".valor as "Preço"
-                    FROM "Cidades"
+    const select = `SELECT "Hoteis".nome as "Hospedagens",
+                        "Companhia".nome as "Companhia",
+                        "Passagens".data_saida as "Data_de_saida",
+                        "Passagens".hora_saida as "Horário_de_saida",
+                        "Passagens".data_chegada as "Data_de_chegada",
+                        "Passagens".hora_chegada as "Horário_previsto_de_chegada",
+                        "Passagens".valor as "Preço_por_passagem"
+                    FROM "Rotas"
+                    JOIN "Cidades" ON "Rotas".destino = "Cidades".id
                     JOIN "Hoteis" ON "Cidades".id = "Hoteis".cidade_id
-                    JOIN "Quartos" ON "Hoteis".id = "Quartos".hotel_id
-                    JOIN "Fotos" ON "Hoteis".id = "Fotos".hotel_id
-                    JOIN "Rotas" ON "Cidades".id = "Rotas".destino
                     JOIN "Passagens" ON "Rotas".id = "Passagens".rota_id
                     JOIN "Companhia" ON "Passagens".companhia_id = "Companhia".id
                     WHERE "Cidades".nome = $1`;
